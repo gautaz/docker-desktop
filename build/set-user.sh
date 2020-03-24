5b8df93be410529340ed38358dae82e018fa816b
gautaz <gautaz.github@gmail.com>
2020-03-25 00:45:11 +0100

    fix build for linux hosts & fix login environment

--- build/set-user.sh
+++ build/set-user.sh
@@ -10,6 +10,7 @@ echo "Creating user ${USERLOGIN}[${USERID}:${GROUPID}]"
 addgroup "${USERLOGIN}"
 sed -i "s#${USERLOGIN}:x:[^:]*:#${USERLOGIN}:x:${GROUPID}:#" /etc/group
 sed -i -e '$!{H;d};$G' /etc/group
-adduser -h /home/user -g "container user" -s /nix/var/nix/profiles/default/bin/bash -G "${USERLOGIN}" -D "${USERLOGIN}"
+adduser -h /home/user -g "container user" -s /bin/bash -G "${USERLOGIN}" -D "${USERLOGIN}"
 sed -i "s#${USERLOGIN}:x:[^:]*:#${USERLOGIN}:x:${USERID}:#" /etc/passwd
 sed -i -e '$!{H;d};$G' /etc/passwd
+chown -R "${USERID}:${GROUPID}" /home/user
