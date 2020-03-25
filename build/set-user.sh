c628fc57d5ca21bedcf845adda7e6824daa29083
gautaz <gautaz.github@gmail.com>
2020-03-25 01:31:24 +0100

    use nix bash

--- build/set-user.sh
+++ build/set-user.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 set -e
 
-USERID="${1}"
-GROUPID="${2}"
-USERLOGIN="${3}"
-
 echo "Creating user ${USERLOGIN}[${USERID}:${GROUPID}]"
 
 addgroup "${USERLOGIN}"
 sed -i "s#${USERLOGIN}:x:[^:]*:#${USERLOGIN}:x:${GROUPID}:#" /etc/group
 sed -i -e '$!{H;d};$G' /etc/group
-adduser -h /home/user -g "container user" -s /bin/bash -G "${USERLOGIN}" -D "${USERLOGIN}"
+adduser -h /home/user -g "container user" -s "${NIXBIN}/bash" -G "${USERLOGIN}" -D "${USERLOGIN}"
 sed -i "s#${USERLOGIN}:x:[^:]*:#${USERLOGIN}:x:${USERID}:#" /etc/passwd
 sed -i -e '$!{H;d};$G' /etc/passwd
 chown -R "${USERID}:${GROUPID}" /home/user
