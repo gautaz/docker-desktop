65ec273891826308c1202cc61f0d73b4e1681417
gautaz <gautaz.github@gmail.com>
2020-04-02 10:15:10 +0200

    Fix user login & environment

--- build/add-user.sh
+++ build/add-user.sh
@@ -2,10 +2,14 @@
 set -e
 
 addgroup "${USERLOGIN}"
-sed -i "s#${USERLOGIN}:x:[^:]*:#${USERLOGIN}:x:${GROUPID}:#" /etc/group
+sed -i "s#^${USERLOGIN}:x:[^:]*:#${USERLOGIN}:x:${GROUPID}:#" /etc/group
 sed -i -e '$!{H;d};$G' /etc/group
 
-adduser -h /home/user -g "container user" -s "/bin/bash" -G "${USERLOGIN}" -D "${USERLOGIN}"
-sed -i "s#${USERLOGIN}:x:[^:]*:#${USERLOGIN}:x:${USERID}:#" /etc/passwd
+adduser -h "/home/${USERLOGIN}" -g "container user" -s "/bin/bash" -G "${USERLOGIN}" -D "${USERLOGIN}"
+sed -i "s#^${USERLOGIN}:x:[^:]*:#${USERLOGIN}:x:${USERID}:#" /etc/passwd
 sed -i -e '$!{H;d};$G' /etc/passwd
-chown -R "${USERID}:${GROUPID}" /home/user
+chown -R "${USERID}:${GROUPID}" "/home/${USERLOGIN}"
+
+sed -i "s#^${USERLOGIN}:[^:]*:#${USERLOGIN}:${PASSWORD}:#" /etc/shadow
+
+echo "${USERLOGIN} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/user
--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+set -e
 
 case "${OSTYPE}" in
 	darwin*)
@@ -13,10 +14,17 @@ case "${OSTYPE}" in
 		;;
 esac
 
+ORIGINAL_STTY="$(stty -g)"
+stty -echo
 cat > .env <<- EOE
 	USERID=${USERID}
 	GROUPID=${GROUPID}
 	USERLOGIN=${LOGNAME}
+	PASSWORD=$(mkpasswd --method=SHA-512 --stdin)
+	TIMEZONE=${TZ:-$(wget -O - -q http://geoip.ubuntu.com/lookup | sed 's#.*<TimeZone>\(.*\)</TimeZone>.*#\1#')}
+	I3THEME=${I3THEME:-default-light}
 EOE
+stty "${ORIGINAL_STTY}"
+echo
 
 DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker-compose build
